981d7325b79db02b7d82c337c19bc2d8bbfae0c2
Shorya gupta <shorya7818@gmail.com>
2026-08-16 20:02:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (66.20%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
+        sort(piles.begin(),piles.end(),greater<int>());
+
+        int al=0,bob=0;
+        for(int i=0;i<piles.size();i++){
+            if(i%2==0){
+                al+=piles[i];
+            }
+            else{
+                bob+=piles[i];
+
+            }
+        }
+
+        if(bob>al){
+            return false;
+        }
         return true;
     }
+
 };
